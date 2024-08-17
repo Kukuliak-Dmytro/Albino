6d11c0c4b1b4d009d2b49cd7b1e8d8a907bda689
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-17 10:57:00 +0300

    Add: Content and Facts sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,9 +5,12 @@ import FeatureCard from "../../components/Cards/FeatureCard/FeatureCard"
 import styles from './Home.module.scss'
 
 import heroImg from '../../assets/heroImg.png'
+
 import featureImg2 from '../../assets/featureImg1.png'
 import featureImg3 from '../../assets/featureImg2.png'
 import featureImg1 from '../../assets/featureImg3.png'
+
+import contentImg1 from '../../assets/contentImg1.png'
 
 
 
@@ -32,9 +35,39 @@ export function Home() {
          </div>
          <div className={styles.featuresWrapper}>
             <div className={styles.featuresBox}>
-               <FeatureCard logoPath={featureImg1} headerText="Project management" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. "/>
-               <FeatureCard logoPath={featureImg2} headerText="Time tracking" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. "/>
-               <FeatureCard logoPath={featureImg3} headerText="Beautiful mobile app" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. "/>
+               <FeatureCard logoPath={featureImg1} headerText="Project management" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. " />
+               <FeatureCard logoPath={featureImg2} headerText="Time tracking" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. " />
+               <FeatureCard logoPath={featureImg3} headerText="Beautiful mobile app" paragraphText="With lots of unique blocks, you can easily build a page without coding. Build your next landing page. " />
+            </div>
+         </div>
+         <div className={styles.contentWrapper1}>
+            <div className={styles.contentBox1}>
+               <div className={styles.contentText1}>
+                  <h2>Getting started with Albino is easier than ever</h2>
+                  <p>With lots of unique blocks, you can easily build a page without coding. Build your next landing page so quickly with Albino.</p>
+                  <Button width={227} height={59}>Get started for free &#8594;</Button>
+               </div>
+               <div className={styles.contentImg1}>
+                  <img src={contentImg1} alt="" />
+               </div>
+
+            </div>
+         </div>
+         <div className={styles.factsWrapper}>
+            <div className={styles.factsBox}>
+               <div className={styles.fact}>
+                  <h1>1M+</h1>
+                  <p>Customers visit Albino every months</p>
+               </div>
+               <div className={styles.fact}>
+                  <h1>93%</h1>
+                  <p>Satisfaction rate from our customers.</p>
+               </div>
+               <div className={styles.fact}>
+                  <h1>4.9</h1>
+                  <p>Average customer ratings out of 5.00!</p>
+               </div>
+
             </div>
          </div>
 
