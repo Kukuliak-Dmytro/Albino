3488880374a5e250d8bf0d3b176953c38f3d98d9
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-17 08:15:42 +0300

    Responsive menu burger

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,15 +1,23 @@
 import styles from './HeaderComponent.module.scss'
 import Button from '../Buttons/Button'
-export default function HeaderComponent(){
-return(
+import { useState } from 'react'
+export default function HeaderComponent() {
+    const [menuCollapsed, setMenuCollapsed]=useState(true);
+    return (
     <div className={styles.headerWrapper}>
     <div className={styles.headerLeft}><h5>Brainwave.io</h5></div>
-    <div className={styles.headerCenter}><ul>
-        <li><a href="">About us</a></li>
-        <li><a href="">Pages</a></li>
-        <li><a href="">Support</a></li>
-        <li><a href="">Contact</a></li></ul></div>
-    <div className={styles.headerRight}><Button width={158}>Get started free</Button></div>
-    </div>
+    <div className={`${styles.headerCenter} ${menuCollapsed ? '' : styles.shown}`}>
+        <ul>
+        <a href=""><li>About us</li></a>
+        <a href=""><li>Pages</li></a>
+        <a href=""><li>Support</li></a>
+        <a href=""><li>Contact</li></a>
+        </ul>
+        </div>
+        <button className={styles.toggleBtn} onClick={()=>setMenuCollapsed(!menuCollapsed)}>dfdX</button>
+
+    <div className={styles.headerRight}>
+        <Button width={158}>Get started free</Button></div>
+    </div >
 )
 }
