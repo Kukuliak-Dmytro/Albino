a2e13c113ce460633e53c76c0e03a0a1d12e1101
Kukuliak-Dmytro <kukuliak.dmytro.clg@chnu.edu.ua>
2024-08-22 11:56:49 +0300

    Add: Contact card click and hover animations

--- src/components/Cards/ContactCard/ContactCard2.tsx
+++ src/components/Cards/ContactCard/ContactCard2.tsx
@@ -1,26 +1,52 @@
 import styles from './ContactCard2.module.scss'
 
-
+import { useRef } from 'react';
 import { BiMailSend } from 'react-icons/bi'
-export default function ContactCard2(){
-    return(
+export default function ContactCard2() {
+    const button = useRef<HTMLButtonElement>(null);
+
+    const animateOnClick = () => {
+        button.current?.classList.add(`${styles.animate}`)
+        setTimeout(() => {
+            button.current?.classList.remove(`${styles.animate}`)
+        }, 1000)
+    }
+    return (
         <div className={styles.info}>
-                        <button>
-                            <div className={styles.slideWrapper}>
-                                <BiMailSend></BiMailSend>
-                            </div>
-                            <div className={styles.slideWrapper}>
-                                <BiMailSend></BiMailSend>
-                            </div>
-                            <div className={styles.slideWrapper}>
-                                <BiMailSend></BiMailSend>
-                            </div>
-                        </button>
-                        <div>
-                            <h5>Call us</h5>
-                            <p>+1-940-394-2948</p>
-                            <p>+1-389-385-3807</p>
-                        </div>
-                    </div>
+            <button onClick={animateOnClick} ref={button}>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+                <div className={styles.slideWrapper}>
+                    <BiMailSend></BiMailSend>
+                </div>
+            </button>
+            <div>
+                <h5>Call us</h5>
+                <p>+1-940-394-2948</p>
+                <p>+1-389-385-3807</p>
+            </div>
+        </div>
     )
 }
--- src/components/Cards/ContactCard/ContactCard3.tsx
+++ src/components/Cards/ContactCard/ContactCard3.tsx
@@ -1,14 +1,25 @@
 import styles from './ContactCard3.module.scss'
-import { BiCurrentLocation } from 'react-icons/bi'
-export default function ContactCard3(){
-    return(        
+import { GrLocation } from "react-icons/gr";
+import { useRef } from 'react';
+export default function ContactCard3() {
+    const button = useRef<HTMLButtonElement>(null);
+
+    const animateOnClick = () => {
+        button.current?.classList.add(`${styles.animate}`)
+        setTimeout(() => {
+            button.current?.classList.remove(`${styles.animate}`)
+        }, 1000)
+    }
+    return (
         <div className={styles.info}>
-        <button><BiCurrentLocation></BiCurrentLocation></button>
-        <div>
-            <h5>Call us</h5>
-            <p>+1-940-394-2948</p>
-            <p>+1-389-385-3807</p>
+            <button onClick={animateOnClick} ref={button}><div className={styles.svgWrapper}>
+                <GrLocation></GrLocation>
+            </div></button>
+            <div>
+                <h5>Call us</h5>
+                <p>+1-940-394-2948</p>
+                <p>+1-389-385-3807</p>
+            </div>
         </div>
-    </div>
     )
 }
